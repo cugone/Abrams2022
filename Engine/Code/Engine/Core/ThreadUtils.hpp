0490533f7282cc545a214e10a7763fb1a45a606a
cugone <cugone@gmail.com>
2022-01-04 10:45:00 -0600

    C++20 includes std::jthread. No need for my version anymore

--- Engine/Code/Engine/Core/ThreadUtils.hpp
+++ Engine/Code/Engine/Core/ThreadUtils.hpp
@@ -3,17 +3,6 @@
 #include <string>
 #include <thread>
 
-struct joinable_thread : public std::thread {
-    ~joinable_thread() {
-        if(joinable()) {
-            join();
-        }
-    }
-    void detach() = delete;
-};
-
-using jthread = joinable_thread;
-
 namespace ThreadUtils {
 void SetThreadDescription(std::thread& thread, const std::string& description) noexcept;
 void SetThreadDescription(std::thread& thread, const std::wstring& description) noexcept;
