a5ab7dd698075844bc7534eee787579dafc1a2c9
cugone <cugone@gmail.com>
2022-06-23 15:45:02 -0500

    Output Stream operator for GraphicsCardDesc delegates to std::format
    TODO: Currently broken

--- Engine/Code/Engine/RHI/RHITypes.cpp
+++ Engine/Code/Engine/RHI/RHITypes.cpp
@@ -3,6 +3,7 @@
 #include "Engine/Math/MathUtils.hpp"
 
 #include <iomanip>
+#include <format>
 #include <type_traits>
 
 bool operator==(const ViewportDesc& a, const ViewportDesc& b) noexcept {
@@ -20,34 +21,8 @@ bool operator!=(const ViewportDesc& a, const ViewportDesc& b) noexcept {
 }
 
 std::ostream& operator<<(std::ostream& out_stream, const GraphicsCardDesc& graphicsCardDesc) noexcept {
-    const auto entry_name_field_width = std::size_t{40u};
-    const auto entry_field_width = std::size_t{35u};
-    std::string name = graphicsCardDesc.Description;
-    out_stream << std::left << std::setw(entry_name_field_width) << "Name:" << std::right << std::setw(entry_field_width) << name << '\n';
-    auto vid = graphicsCardDesc.VendorId;
-    out_stream << std::hex << std::uppercase;
-    out_stream << std::left << std::setw(entry_name_field_width) << "Vendor ID:" << std::right << std::setw(entry_field_width) << vid << '\n';
-    auto did = graphicsCardDesc.DeviceId;
-    out_stream << std::left << std::setw(entry_name_field_width) << "Device ID:" << std::right << std::setw(entry_field_width) << did << '\n';
-    auto subsysid = graphicsCardDesc.SubSysId;
-    out_stream << std::left << std::setw(entry_name_field_width) << "Subsystem ID:" << std::right << std::setw(entry_field_width) << subsysid << '\n';
-    auto revision = graphicsCardDesc.Revision;
-    out_stream << std::left << std::setw(entry_name_field_width) << "Revision:" << std::right << std::setw(entry_field_width) << revision << '\n';
-    out_stream << std::dec << std::nouppercase;
-    auto ded_vid_mem = graphicsCardDesc.DedicatedVideoMemory;
-    out_stream << std::left << std::setw(entry_name_field_width) << "Video Memory:" << std::right << std::setw(entry_field_width) << std::fixed << std::setprecision(1) << static_cast<long double>(ded_vid_mem) * MathUtils::GIB_BYTES_RATIO.num / MathUtils::GIB_BYTES_RATIO.den << " GB\n";
-    auto ded_sys_mem = graphicsCardDesc.DedicatedSystemMemory;
-    out_stream << std::left << std::setw(entry_name_field_width) << "System Memory:" << std::right << std::setw(entry_field_width) << std::fixed << std::setprecision(1) << static_cast<long double>(ded_sys_mem) * MathUtils::GIB_BYTES_RATIO.num / MathUtils::GIB_BYTES_RATIO.den << " GB\n";
-    auto shared_mem = graphicsCardDesc.SharedSystemMemory;
-    out_stream << std::left << std::setw(entry_name_field_width) << "Shared System Memory:" << std::right << std::setw(entry_field_width) << std::fixed << std::setprecision(1) << static_cast<long double>(shared_mem) * MathUtils::GIB_BYTES_RATIO.num / MathUtils::GIB_BYTES_RATIO.den << " GB\n";
-    bool is_software = graphicsCardDesc.is_software;
-    bool is_unspecified = graphicsCardDesc.is_unspecified;
-    out_stream << std::left << std::setw(entry_name_field_width) << "Adapter Type:" << std::right << std::setw(entry_field_width);
-    if(!is_unspecified) {
-        out_stream << (!is_software ? "Hardware" : "Software");
-    } else {
-        out_stream << "Unknown";
-    }
+    //TODO: Fix formatter
+    out_stream << std::format("{}", graphicsCardDesc);
     return out_stream;
 }
 
