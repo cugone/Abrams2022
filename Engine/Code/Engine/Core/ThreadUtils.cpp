133497b5d8997a47b61018600ced4a25341761fd
cugone <cugone@gmail.com>
2022-08-17 22:42:34 -0500

    Add std::jthread overloads to ThreadUtils

--- Engine/Code/Engine/Core/ThreadUtils.cpp
+++ Engine/Code/Engine/Core/ThreadUtils.cpp
@@ -33,4 +33,31 @@ void GetThreadDescription(std::thread& thread, std::wstring& description) noexce
 #endif
 }
 
+void SetThreadDescription(std::jthread& thread, const std::string& description) noexcept {
+    auto wide_description = StringUtils::ConvertMultiByteToUnicode(description);
+    SetThreadDescription(thread, wide_description);
+}
+
+void SetThreadDescription(std::jthread& thread, const std::wstring& description) noexcept {
+#ifdef PLATFORM_WINDOWS
+    ::SetThreadDescription(thread.native_handle(), description.c_str());
+#endif
+}
+
+void GetThreadDescription(std::jthread& thread, std::string& description) noexcept {
+    std::wstring wide_description{};
+    GetThreadDescription(thread, wide_description);
+    description = StringUtils::ConvertUnicodeToMultiByte(wide_description);
+}
+
+void GetThreadDescription(std::jthread& thread, std::wstring& description) noexcept {
+#ifdef PLATFORM_WINDOWS
+    PWSTR d{};
+    ::GetThreadDescription(thread.native_handle(), &d);
+    description.assign(d);
+    ::LocalFree(d);
+    d = nullptr;
+#endif
+}
+
 } // namespace ThreadUtils
--- Engine/Code/Engine/Core/ThreadUtils.hpp
+++ Engine/Code/Engine/Core/ThreadUtils.hpp
@@ -8,4 +8,8 @@ void SetThreadDescription(std::thread& thread, const std::string& description) n
 void SetThreadDescription(std::thread& thread, const std::wstring& description) noexcept;
 void GetThreadDescription(std::thread& thread, std::string& description) noexcept;
 void GetThreadDescription(std::thread& thread, std::wstring& description) noexcept;
+void SetThreadDescription(std::jthread& thread, const std::string& description) noexcept;
+void SetThreadDescription(std::jthread& thread, const std::wstring& description) noexcept;
+void GetThreadDescription(std::jthread& thread, std::string& description) noexcept;
+void GetThreadDescription(std::jthread& thread, std::wstring& description) noexcept;
 } // namespace ThreadUtils
