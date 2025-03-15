ba2b04255e6e9f68c0f5edcc824b7c2b536c2e9d
cugone <cugone@gmail.com>
2025-03-15 06:25:39 -0500

    Implements Clay class

--- Engine/Code/Engine/UI/UISystem.hpp
+++ Engine/Code/Engine/UI/UISystem.hpp
@@ -21,8 +21,7 @@
 #endif
 
 #include "Engine/UI/DearImgui.hpp"
-
-#include <Thirdparty/clay/clay.h>
+#include "Engine/UI/ClayUI.hpp"
 
 #include <filesystem>
 #include <functional>
@@ -42,7 +41,7 @@ public:
     UISystem(UISystem&& other) = default;
     UISystem& operator=(const UISystem& other) = default;
     UISystem& operator=(UISystem&& other) = default;
-    virtual ~UISystem() noexcept;
+    virtual ~UISystem() noexcept = default;
     virtual void Initialize() noexcept override;
     virtual void BeginFrame() noexcept override;
     virtual void Update(TimeUtils::FPSeconds deltaSeconds) noexcept override;
@@ -70,27 +69,6 @@ public:
 
 protected:
 private:
-
-    void ClayInit() noexcept;
-    void ClayUpdate(TimeUtils::FPSeconds deltaSeconds) noexcept;
-    void ClayRender() const noexcept;
-
-    void ClayDeinitialize() noexcept;
-
-    mutable Camera2D m_ui_camera{};
     DearImgui m_imgui{};
-    std::function<void()> m_clayLayoutCallback{};
-    mutable Clay_RenderCommandArray m_clay_commands{};
-    Clay_Context* m_clayContext{};
-    std::unique_ptr<char[]> m_clayMemoryBlock{};
-    float m_clayScrollSpeed{10.0f};
-    bool m_show_clay_debug_window = false;
+    ClayUI m_clay{};
 };
-
-namespace Clay {
-Clay_Color RgbaToClayColor(Rgba color) noexcept;
-Clay_String StrToClayString(const std::string& str) noexcept;
-Clay_Dimensions Vector2ToClayDimensions(Vector2 v) noexcept;
-Clay_Vector2 Vector2ToClayVector2(Vector2 v) noexcept;
-Rgba ClayColorToRgba(Clay_Color textColor) noexcept;
-}
